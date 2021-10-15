159f170e839248e455adab094a88f9fa6b54a087
FelipeMD <felipe.macieldsouza@gmail.com>
2021-10-15 14:11:42 -0300

    estruturando o projeto e ligando ao bd

---
+++ CPAPP.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.Configuration;
+using CPAPP.Infastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
+
+namespace CPAPP.CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("MSSQLServerConnectionString"));
+
+            return services;
+        }
+    }
+}
---
+++ CPAPP.Infastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CPAPP.Infastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
